d4445f73989b747c45267113c4b910120ba0cb2b
Dominik Wagner <dominik.r.mall2@gmail.com>
2021-05-25 12:11:15 +0200

    Trainer class and relevant methods filled

---
+++ TaxiFareModel/trainer.py
@@ -0,0 +1,76 @@
+# imports
+from TaxiFareModel.data import get_data, clean_data, holdout_data
+from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
+from TaxiFareModel.utils import compute_rmse
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LinearRegression
+
+
+class Trainer():
+    def __init__(self, X, y):
+        """
+            X: pandas DataFrame
+            y: pandas Series
+        """
+        self.pipeline = None
+        self.X = X
+        self.y = y
+
+    def build_preproc_pipe(self):
+        dist_pipe = Pipeline([
+            ('dist_trans', DistanceTransformer()),
+            ('stdscaler', StandardScaler())
+        ])
+        time_pipe = Pipeline([
+            ('time_enc', TimeFeaturesEncoder('pickup_datetime')),
+            ('ohe', OneHotEncoder(handle_unknown='ignore'))
+        ])
+        
+        preproc_pipe = ColumnTransformer([
+            ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
+            ('time', time_pipe, ['pickup_datetime'])
+        ], remainder="drop")
+
+        return preproc_pipe
+
+    def set_pipeline(self):
+        """defines the pipeline as a class attribute"""
+
+        preproc_pipe = self.build_preproc_pipe()
+        self.pipeline = Pipeline([
+            ('preproc', preproc_pipe),
+            ('linear_model', LinearRegression())
+        ])
+
+        return self.pipeline
+
+    def run(self):
+        """set and train the pipeline"""
+        self.pipeline.fit(self.X, self.y)
+
+        return self.pipeline
+
+    def evaluate(self, X_test, y_test):
+        """evaluates the pipeline on df_test and return the RMSE"""
+        y_pred = self.pipeline.predict(X_test)
+        rmse = compute_rmse(y_pred, y_test)
+
+        return rmse
+
+
+if __name__ == "__main__":
+    # get data
+    df = get_data()
+    # clean data
+    df = clean_data(df)
+    # set X and y
+    X_train, X_test, y_train, y_test = holdout_data(df)
+    # train
+    trainer = Trainer(X_train, y_train)
+    trainer.set_pipeline()
+    trainer.run()
+    # evaluate
+    rmse = trainer.evaluate(X_test, y_test)
+    print(f'rmse: {rmse}')
